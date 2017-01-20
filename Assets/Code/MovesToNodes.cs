9b85c080c54471342c075815eba54f517ee139b7
GP <gp.gooiker@gmail.com>
2017-01-21 00:39:51 +0100

    Add a prefab for a node that moves to other nodes

---
+++ Assets/Code/MovesToNodes.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(Rigidbody))]
+
+public class MovesToNodes : MonoBehaviour
+{
+	public float attractionForce = 0.003f;
+	private Rigidbody body;
+
+	void Start ()
+	{
+		attractionForce = 0.003f;
+		body = this.gameObject.GetComponent<Rigidbody> ();
+	}
+
+	void Update ()
+	{
+		getOtherNearbyNodesWithTag ("Node");
+	}
+
+	private void getOtherNearbyNodesWithTag (string tag)
+	{
+		List<GameObject> allNodes = new List<GameObject> (GameObject.FindGameObjectsWithTag (tag));
+		List<GameObject> otherNodes = new List<GameObject> (allNodes);
+
+		otherNodes.RemoveAll (item => item.name == gameObject.name);
+
+		foreach (GameObject otherNode in otherNodes) {
+			float distanceToOtherNode = Vector3.Distance (otherNode.transform.position, gameObject.transform.position);
+			float forceMagnitude = 0;
+			Vector3 forceToOtherNode;
+			Rigidbody otherBody;
+			Vector3 toOtherNode;
+
+			if (distanceToOtherNode > 0) {
+				forceMagnitude = ((1 / distanceToOtherNode) + 1) * attractionForce;
+			}
+
+			if (forceMagnitude > 0) {
+				toOtherNode = otherNode.transform.position - gameObject.transform.position;
+				forceToOtherNode = toOtherNode * forceMagnitude;
+				body.AddForce(forceToOtherNode); 
+			}
+		}
+	}
+}
