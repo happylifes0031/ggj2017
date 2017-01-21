92ed612e15c18cfad1ea8a6842f0a4854f532352
GP <gp.gooiker@gmail.com>
2017-01-21 11:59:25 +0100

    Add a single, global state

---
+++ Assets/Code/State/GameState.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Attach this script to an empty GameObject called GameState. Then, it can be
+/// accessed with GameObject.Find ("GameState").GetComponent<GameState> ();
+/// </summary>
+public class GameState : MonoBehaviour
+{
+	public PlayerWaves playerWaves; 
+	public static GameState gameState;
+
+	void Awake() {
+		// Make sure there's only one instance of the game state
+		gameState = this;
+
+		if (gameState.playerWaves == null) {
+			gameState.playerWaves = new PlayerWaves ();
+		}
+	}
+}
