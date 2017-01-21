7e0f4b3381dfb37b9659e48e9b32c12f5c4fb54a
GP <gp.gooiker@gmail.com>
2017-01-21 10:01:41 +0100

    Add a repulsion force

--- Assets/Code/MovesToNodes.cs
+++ Assets/Code/MovesToNodes.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 
 public class MovesToNodes : MonoBehaviour
 {
-	public float attractionForce = 0.003f;
+	public float AttractionForce = 0.025f;
 	private Rigidbody body;
 
 	void Start ()
@@ -16,30 +16,26 @@ public class MovesToNodes : MonoBehaviour
 
 	void Update ()
 	{
-		getOtherNearbyNodesWithTag ("Node");
+		moveToNodesWithTag ("Node");
 	}
 
-	private void getOtherNearbyNodesWithTag (string tag)
+	private void moveToNodesWithTag (string tag)
 	{
 		List<GameObject> allNodes = new List<GameObject> (GameObject.FindGameObjectsWithTag (tag));
-		List<GameObject> otherNodes = new List<GameObject> (allNodes);
 
-		otherNodes.RemoveAll (item => item.name == gameObject.name);
-
-		foreach (GameObject otherNode in otherNodes) {
-			float distanceToOtherNode = Vector3.Distance (otherNode.transform.position, gameObject.transform.position);
+		foreach (GameObject node in allNodes) {
+			float distanceToOtherNode = Vector3.Distance (node.transform.position, gameObject.transform.position);
 			float forceMagnitude = 0;
 
 			if (distanceToOtherNode > 0) {
-				forceMagnitude = ((1 / distanceToOtherNode) + 1) * attractionForce;
+				forceMagnitude = ((1 / distanceToOtherNode) + 1) * AttractionForce;
 			}
 
 			if (forceMagnitude > 0) {
-				Vector3 forceToOtherNode, toOtherNode;
+				Vector3 toOtherNode = node.transform.position - gameObject.transform.position;
+				Vector3 forceToOtherNode = toOtherNode * forceMagnitude;
 
-				toOtherNode = otherNode.transform.position - gameObject.transform.position;
-				forceToOtherNode = toOtherNode * forceMagnitude;
-				body.AddForce(forceToOtherNode); 
+				body.AddForce(forceToOtherNode);
 			}
 		}
 	}
---
+++ Assets/Code/PushesOtherNodes.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(Rigidbody))]
+
+public class PushesOtherNodes : MonoBehaviour
+{
+	public float PushForce = 0.06f;
+	public float MaximumPushDistance = 4f;
+
+	void Update ()
+	{
+		pushOtherNodesWithTag ("Node");
+	}
+
+	private void pushOtherNodesWithTag (string tag)
+	{
+		List<GameObject> allNodes = new List<GameObject> (GameObject.FindGameObjectsWithTag (tag));
+
+		foreach (GameObject node in allNodes) {
+			float distanceToOtherNode = Vector3.Distance (node.transform.position, gameObject.transform.position);
+			float forceMagnitude = 0;
+
+			if (distanceToOtherNode > 0 && distanceToOtherNode < MaximumPushDistance) {
+				Rigidbody otherBody = node.GetComponent<Rigidbody> ();
+				Vector3 toOtherNode = node.transform.position - gameObject.transform.position;
+				forceMagnitude = ((1 / distanceToOtherNode) + 1) * PushForce;
+				Vector3 impulseToOtherNode = toOtherNode * forceMagnitude;
+
+				if (otherBody != null) {
+					otherBody.AddForce (impulseToOtherNode);
+				}
+			}
+		}
+	}
+}
